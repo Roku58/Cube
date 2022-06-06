22436d2c6353e09b1a886cce16b1c16b266097e6
Roku <mr558869193@gmail.com>
2022-06-06 13:17:04 +0900

    no message

---
+++ Assets/Script/Interface/ISkill.cs
@@ -0,0 +1,37 @@
+public interface ISkill
+{
+    /// <summary>スキルのID</summary>
+    SkillID ID { get; }
+
+    /// <summary>スキルのタイプ</summary>
+    SkillType Type { get; }
+    
+
+    /// <summary>スキルのレベルが最大かどうか</summary>
+    bool IsLevelMax { get; }
+
+    /// <summary>初回取得時に呼ぶ</summary>
+    void SetUp();
+
+    /// <summary>毎フレーム呼ぶ</summary>
+    void Update();
+
+    /// <summary>二回目以降の取得時に呼ぶ</summary>
+    void LevelUp();
+
+    /// <summary>スキル削除時に呼ぶ</summary>
+    void Delete();
+}
+
+
+public enum SkillID
+{
+    Gun = 0,
+    AddHP = 1,
+}
+
+public enum SkillType
+{
+    Active = 0,
+    Passive = 1,
+}
