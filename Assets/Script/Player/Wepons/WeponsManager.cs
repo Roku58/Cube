8a72fa41dcca7f6524835e6713230924577e6018
Roku <mr558869193@gmail.com>
2022-06-08 04:48:28 +0900

    WeponsManager

--- Assets/Script/Player/Wepons/WeponsManager.cs
+++ Assets/Script/Player/Wepons/WeponsManager.cs
@@ -4,17 +4,46 @@ using UnityEngine;
 
 public class WeponsManager : MonoBehaviour
 {
-    [SerializeField] GameObject[] wepons;
-    [SerializeField] GameObject[] weponLevel;
+    public WeponLeve[] wepons;
+    float _timer;
 
-    void Start()
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
+        _timer += Time.deltaTime;
+        if (_timer > 3)
+        {
+            StartCoroutine("WeponActive");
+            _timer = 0;
+        }
+
     }
+
+    IEnumerator WeponActive()
+    {
+        for (int i = 0; i < wepons.Length; i++)
+        {
+            if (wepons[i].weponLevelIndex > 0)
+            {
+                wepons[i].weponLevel[wepons[i].weponLevelIndex - 1].SetActive(true);
+                yield return new WaitForSeconds(2);
+                wepons[i].weponLevel[wepons[i].weponLevelIndex - 1].SetActive(false);
+            }
+        }
+    }
+
+}
+
+[System.Serializable]
+public class WeponLeve
+{
+    public string weponName;
+    public GameObject wepon;
+    public GameObject[] weponLevel;
+    public int weponLevelIndex;
 }
